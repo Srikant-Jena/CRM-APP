8f9c74707e32dcc67a00b82a414444fa80833264
Srikant jena <srikant.3348@gmail.com>
2023-06-28 17:47:35 +0530

    Initial commit

---
+++ djcrm/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from Leads.views import home_page
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),                                            
+    path('', home_page),                                            
+]
