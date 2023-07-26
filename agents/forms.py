b3098b0292ecaae0fe06776ac01617f776606fb2
Srikant jena <srikant.3348@gmail.com>
2023-07-26 16:46:34 +0530

    Fifth commit

--- agents/forms.py
+++ agents/forms.py
@@ -1,16 +1,11 @@
 from django import forms
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
-
-User = get_user_model()
+from leads.models import Agent
 
 
 class AgentModelForm(forms.ModelForm):
     class Meta:
-        model = User
+        model = Agent
         fields = (
-            'email',
-            'username',
-            'first_name',
-            'last_name'
+            'user',
+           
         )
